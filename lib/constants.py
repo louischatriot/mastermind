30e4482195913df1accf249336fe6d62b4d5a8a0
Louis Chatriot <louis.chatriot@gmail.com>
2020-08-08 18:36:35 +0200

    Colored output

--- lib/constants.py
+++ lib/constants.py
@@ -1,1 +1,11 @@
 colors = ["W", "P", "G", "R", "B", "Y"]
+
+color_names = {
+    "W": "white",
+    "P": "magenta",
+    "G": "green",
+    "R": "red",
+    "B": "blue",
+    "Y": "yellow",
+}
+
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import json
 from lib.game import Game
 from lib.utils import clear
-from lib.constants import colors
+from lib.constants import colors, color_names
+from termcolor import colored
 
 
 game = Game(4, 6)
 
 tries = []
-
 
 
 while True:
@@ -16,13 +16,9 @@ while True:
     for l in tries:
         ok, near = game.test(l)
         res = ["\u25cf"] * ok + ["\u25cb"] * near + [" "] * (len(game.target) - ok - near)
-
-
-        line = "  ".join(res) + "    -    " + "  ".join(l)
-
-
-
+        line = "  ".join(res) + "    -    " + "  ".join([colored("\u25cf", color_names[it]) for it in l])
         print(line)
+        print("")
 
     print("")
     print("")
